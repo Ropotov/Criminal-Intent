690d0f32862376c0985eb362234cfc9892ff173a
Nick <Ropotov.dev@gmail.com>
2021-08-16 21:03:16 +0400

    started creating a database

---
+++ app/src/main/java/database/CrimeDao.kt
@@ -0,0 +1,17 @@
+package database
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.bignerdranch.android.criminal_intent.Crime
+import java.util.*
+
+@Dao
+interface CrimeDao {
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): List<Crime>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): Crime?
+
+
+}
