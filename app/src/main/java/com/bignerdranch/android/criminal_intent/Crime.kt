9d2a97655bd1a580a5add15f7582a959f9923308
Nick <Ropotov.dev@gmail.com>
2021-08-18 18:27:54 +0400

    Added the ability to select a suspect from the contact book and send a crime report

--- app/src/main/java/com/bignerdranch/android/criminal_intent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminal_intent/Crime.kt
@@ -3,9 +3,12 @@ package com.bignerdranch.android.criminal_intent
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.*
+
 @Entity
-data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
+data class Crime(
+    @PrimaryKey val id: UUID = UUID.randomUUID(),
     var title: String = "",
     var date: Date = Date(),
-    var isSolved: Boolean = false) {
-}
+    var isSolved: Boolean = false,
+    var suspect: String = ""
+)
